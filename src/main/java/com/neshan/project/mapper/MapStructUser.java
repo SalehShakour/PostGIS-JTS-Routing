325e5ffb1f7041fd925201acf179eab727c87a81
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-09-14 16:12:04 +0330

    - Implementation MapStructUser

---
+++ src/main/java/com/neshan/project/mapper/MapStructUser.java
@@ -0,0 +1,12 @@
+package com.neshan.project.mapper;
+
+import com.neshan.project.domain.User;
+import com.neshan.project.dto.UserDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface MapStructUser {
+    User userDtoToUser(UserDTO userDTO);
+    UserDTO userToUserDTO(User user);
+}
