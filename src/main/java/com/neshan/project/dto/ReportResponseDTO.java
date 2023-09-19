09de37d8a2c1e84708966a55e6e53abdc0714cd2
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-09-19 12:37:12 +0330

    Add ID field to ReportDTO

--- src/main/java/com/neshan/project/dto/ReportResponseDTO.java
+++ src/main/java/com/neshan/project/dto/ReportResponseDTO.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @AllArgsConstructor
 public class ReportResponseDTO {
 
+    private Long id;
     private ReportType type;
     private String point;
     private Map<String, Object> additionalInformation;
